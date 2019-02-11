46be394976e33bab977456ee71c7ebdb731ff0f0
Craig Cerny <cerny01@gmail.com>
2019-02-10 20:26:52 -0500

    Refactor code into functions

--- snapper/snapper.py
+++ snapper/snapper.py
@@ -1,8 +1,11 @@
 import boto3
 
-if __name__ == 'main':
-    session = boto3.Session(profile_name='snapper')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='snapper')
+ec2 = session.resource('ec2')
 
+def list_instances():
     for i in ec2.instances.all():
         print(i)
+
+if __name__ == 'main':
+    list_instances()
